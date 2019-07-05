df90b81ebf6712c38a90704192567bf0e5423b74
AlGeBrig <avenon7@gmail.com>
2019-07-05 14:52:12 +0300

    2 commit

--- carnitine.rb
+++ carnitine.rb
@@ -1,12 +1,12 @@
 puts "Программа рассчитывает количество 1 порции л-карнитина в зависимости от концентрации вещества в бутылки"
 puts "Введите объем 1 порции (мл)"
-con = gets.chomp.to_i
+con = gets.chomp.to_f
 
 puts "Введите количество вещества в одной порции"
-portion = gets.chomp.to_i
+portion = gets.chomp.to_f
 
 puts "Введите необходимое количество вещества в одной порции"
-need = gets.chomp.to_i
+need = gets.chomp.to_f
 
 need_portion = con * need / portion
-puts "Вам нужна порция в #{need_portion}"
+puts "Вам нужна порция в #{need_portion} мл"
