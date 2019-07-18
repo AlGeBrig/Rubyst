f06f58bff86df6f6c7718f6268d0eb11f3035072
AlGeBrig <avenon7@gmail.com>
2019-07-18 20:29:36 +0300

    new program

---
+++ format_sequence.rb
@@ -0,0 +1,6 @@
+puts format("%12s  |  %s ", "Product", "Cost in Cents")
+puts "-" * 36
+puts format("%12s | %6.3f", "Stamps", 55.32230)
+puts format("%12s | %.1f", "Paper Climps", 546.324234)
+puts format("%12s | %9.5f", ":", 99.9)
+
